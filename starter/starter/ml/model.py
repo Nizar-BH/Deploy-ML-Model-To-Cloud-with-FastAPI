ec95a192d3a8b4de30cfa06196237ef879d41415
nb.hmida <nb.hmida@instadeep.com>
2023-11-10 17:55:28 +0100

    Feat: add first implementation of the model file

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,10 +1,10 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
 
-
-# Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
     """
-    Trains a machine learning model and returns it.
+    Trains a machine learning model with GridSearchCV and returns it.
 
     Inputs
     ------
@@ -12,13 +12,16 @@ def train_model(X_train, y_train):
         Training data.
     y_train : np.array
         Labels.
+
     Returns
     -------
     model
         Trained machine learning model.
     """
 
-    pass
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
+    return model
 
 
 def compute_model_metrics(y, preds):
@@ -48,13 +51,64 @@ def inference(model, X):
 
     Inputs
     ------
-    model : ???
+    model : object
         Trained machine learning model.
     X : np.array
         Data used for prediction.
+
     Returns
     -------
     preds : np.array
         Predictions from the model.
     """
-    pass
+    # Use the trained model to make predictions
+    preds = model.predict(X)
+    return preds
+
+
+def compute_slices(df, feature, y, preds):
+    """
+    Compute the performance on slices for a given categorical feature.
+
+    Inputs:
+    df: pandas DataFrame
+        Test dataframe pre-processed with features, including the categorical feature for slicing
+        then save the dataframe into a text file appending each time for each feature.
+    feature: str
+        Feature on which to perform the slices.
+    y : np.array
+        Corresponding known labels, binarized.
+    preds : np.array
+        Predicted labels, binarized.
+
+    Returns:
+    Dataframe with columns:
+        feature value: value of the categorical feature
+        n_samples: number of data samples in the slice
+        precision : precision score
+        recall : recall score
+        fbeta : fbeta score
+    """
+    slice_options = df[feature].unique()
+    performance_data = []
+    save_path = './slice_output.txt'
+
+    for option in slice_options:
+        slice_mask = df[feature] == option
+        slice_y = y[slice_mask]
+        slice_preds = preds[slice_mask]
+
+        precision = precision_score(slice_y, slice_preds)
+        recall = recall_score(slice_y, slice_preds)
+        fbeta = fbeta_score(slice_y, slice_preds, beta=1)
+
+        performance_data.append({
+            'feature value': option,
+            'n_samples': len(slice_y),
+            'precision': precision,
+            'recall': recall,
+            'fbeta': fbeta
+        })
+
+    performance_df = pd.DataFrame(performance_data)
+    performance_df.to_csv(save_path, mode='a', index=False)
